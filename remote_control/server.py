24ec2268cd0a2141462d2e9e7b2c004a1e845088
makovako <valko.marek@gmail.com>
2020-12-26 20:07:09 +0000

    Added functionality to turn on/off, change brightness, added more colors, added base for effects

--- remote_control/server.py
+++ remote_control/server.py
@@ -13,6 +13,8 @@ static_files = {
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files=static_files)
 
+curr_brightness = 0.1
+
 
 @sio.event
 def tree(sid, data):
@@ -20,10 +22,48 @@ def tree(sid, data):
     global tree
     tree.color = Color(data["color"])
 
+@sio.event
+def color(sid, color):
+    global tree
+    tree.color = Color(color)
+
+@sio.event
+def plus(sid):
+    global tree
+    global curr_brightness
+    if curr_brightness <= 0.9:
+        curr_brightness = 0.1 + curr_brightness
+        tree.brightness = curr_brightness
+        print(tree.brightness)
+
+@sio.event
+def minus(sid):
+    global tree
+    global curr_brightness
+    if curr_brightness >= 0.1:
+        curr_brightness = curr_brightness - 0.1
+        tree.brightness = curr_brightness
+        print(tree.brightness)
+
+@sio.event
+def on(sid):
+    global tree
+    tree.on()
+
+@sio.event
+def off(sid):
+    global tree
+    tree.off()
+
+@sio.event
+def effect(sid, effect):
+    global tree
+    print(effect)
+
 tree = RGBXmasTree()
 
 if __name__ == "__main__":
-    tree.brightness = 0.1
+    tree.brightness = curr_brightness
     tree.color = (1,1,1)
     eventlet.wsgi.server(eventlet.listen(('',5000)), app)
     tree.off()
